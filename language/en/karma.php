8d1e22b44f94c5d324f3717bdefbb9a7b9f33a0d
Vinícius <vinnykun@hotmail.com>
2026-08-06 20:14:46 -0300

    Fix code sniffer error

--- language/en/karma.php
+++ language/en/karma.php
@@ -69,4 +69,3 @@ $lang = array_merge($lang, array(
 // Report Reasons
 $lang['report_reasons']['TITLE']['ABUSE_KARMA'] = 'Vote abuse';
 $lang['report_reasons']['DESCRIPTION']['ABUSE_KARMA'] = 'Manipulating votes or targeting users (targeted upvoting or downvoting).';
-
